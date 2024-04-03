e154deca442d517f5b3b18ef68b4565466449fb4
morsalinamunmun <morsalinamunmun@gmail.com>
2024-04-03 10:49:35 +0600

    send task data db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,7 @@ app.use(express.json());
 //password yGqko1lpqG6TmSSf
 //username: coderSequedDB
 
-const uri = "mongodb+srv://coderSequedDB:yGqko1lpqG6TmSSf@cluster0.ddlqajr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ddlqajr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,19 +28,26 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
     const userCollection = client.db("coderSequedDB").collection("users");
+    const taskCollection = client.db("coderSequedDB").collection("task");
 
-  
-      //user 
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query)
-        if (existingUser) {
-          return res.send({ message: 'user Already add', insertedId: null })
-        }
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-      })
+    //user 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user Already add', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    //add task
+    app.post('/task', async (req, res) => {
+      const result = await taskCollection.insertOne(req.body);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -52,10 +60,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('coderSqued is sitting')
+app.get('/', (req, res) => {
+  res.send('coderSqued is sitting')
 })
 
-app.listen(port, () =>{
-    console.log(`coderSqued is sitting on port ${port}`)
+app.listen(port, () => {
+  console.log(`coderSqued is sitting on port ${port}`)
 })
